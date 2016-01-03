09768719c777bdc46b49993ba27c23d000fb75dd
Gabe Heafitz <gabe@shopkeep.com>
2016-01-02 23:48:13 -0500

    Remove Hello World route.

--- spec/integration/yellowtail_spec.rb
+++ spec/integration/yellowtail_spec.rb
@@ -1,11 +1,6 @@
 require File.expand_path '../../spec_helper.rb', __FILE__
 
 describe 'Yellowtail main app' do
-  it 'allows accessing the home page' do
-    get '/'
-    expect(last_response).to be_ok
-  end
-
   it 'returns the average word length from /words/avg_len' do
     post '/words/avg_len', "{\"text\":\"My cat is grey. It doesn\'t have feathers.\"}"
     expect(last_response.body).to eq '3.875'
--- yellowtail.rb
+++ yellowtail.rb
@@ -1,10 +1,6 @@
 require 'sinatra'
 require 'json'
 
-get '/' do
-  'Hello, World!'
-end
-
 post '/words/avg_len' do
   body = JSON.parse(request.body.string)['text']
   result = AverageLength.calculate(RemoveIrrelevantCharactersFilter.filter(WordTokenizer.tokenize(body)))
